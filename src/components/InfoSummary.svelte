4e5be73081770be543dc9cafe028f229726968ad
Tulbu Vadim <tulbu.vadim@gmail.com>
2024-03-05 13:35:48 +0200

    Fix css applied to paragraph

--- src/components/InfoSummary.svelte
+++ src/components/InfoSummary.svelte
@@ -21,21 +21,4 @@
 		grid-template-rows: 1fr 1fr;
 		gap: 12px;
 	}
-
-	:global(p:last-of-type) {
-		grid-row: 1/3;
-		color: #26c2ae;
-		font-size: 32px;
-		letter-spacing: 0.67px;
-		text-align: center;
-		margin: auto;
-		font-weight: 700;
-	}
-
-	:global(p:first-of-type) {
-		color: #ffffff;
-		font-size: 16px;
-		font-weight: 700;
-		margin: 0;
-	}
 </style>
--- src/components/Summary.svelte
+++ src/components/Summary.svelte
@@ -26,4 +26,21 @@
 		flex-direction: column;
 		gap: 24px;
 	}
+
+	p:last-of-type {
+		grid-row: 1/3;
+		color: #26c2ae;
+		font-size: 32px;
+		letter-spacing: 0.67px;
+		text-align: center;
+		margin: auto;
+		font-weight: 700;
+	}
+
+	p:first-of-type {
+		color: #ffffff;
+		font-size: 16px;
+		font-weight: 700;
+		margin: 0;
+	}
 </style>
